341a37a14b2c91c77f2171a4a33988cf95ef81f5
Tina Hu <tinahu198803@gmail.com>
2026-04-02 23:24:08 +0800

    fix: 添加webpack路径别名配置修复@模块解析

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -9,4 +11,13 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
+    return config;
+  },
+};
